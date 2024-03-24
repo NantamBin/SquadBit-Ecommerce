23663e08fa82e399cf7ddf3308bd0eea7f08415d
NantamBin <jvjobness@protonmail.com>
2024-03-23 22:27:44 -0400

    [bugfix] - consertar hello world

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-//Middleware
+// CORS Middleware
 app.use((req, res, next) => {
-    app.use((req, res, next)=>{
-        res.setHeader('Acess-Control-Allow-Origin', 'http://localhost:3000/');
-        res.setHeader('Acess-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Acess-Control-Allow-Header', 'Content-Type, Authorization');
-        next();
-    })
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
 //Rota
