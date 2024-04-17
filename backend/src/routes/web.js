72d6881ed124d1bdba1b1b3dea4b78c8e814ef98
OtnielSSilva <otniel.dss04@gmail.com>
2024-04-17 02:36:17 -0400

    feat: implement search functionality with getProductByName route and controller

--- backend/src/routes/web.js
+++ backend/src/routes/web.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Controllers
-const productsController = require('../controllers/productsController.js');
-const homeController = require('../controllers/homeController.js');
+const productsController = require("../controllers/productsController.js");
+const homeController = require("../controllers/homeController.js");
 
-/* Route for HOME*/
-router.get('/', homeController.get);
+/* Route for HOME */
+router.get("/", homeController.get);
 
-/* Route for Products*/
-router.get('/meusProdutos', productsController.get);
-router.post('/meusProdutos', productsController.create);
-router.put('/meusProdutos/:id', productsController.update);
-router.delete('/meusProdutos/:id', productsController.remove);
+/* Route for Products */
+router.get("/meusProdutos", productsController.get);
+router.post("/meusProdutos", productsController.create);
+router.put("/meusProdutos/:id", productsController.update);
+router.delete("/meusProdutos/:id", productsController.remove);
+
+/* Route for Product serch */
+router.get("/meusProdutos/:name", productsController.getByName);
 
 module.exports = router;
