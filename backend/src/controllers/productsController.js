286fda54fb9a02a9cbd5b63e4dfe161ab802db4f
OtnielSSilva <otniel.dss04@gmail.com>
2024-04-28 02:46:54 -0400

    [feature] - adiciona busca de produtos por nome

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,44 +1,54 @@
-const produtoService = require('../service/productsService.js');
+const produtoService = require("../service/productsService.js");
 
 async function get(req, res, next) {
-  try {
-      res.json(await produtoService.getProdutos(req.query.page));
-  } catch (error) {
-      console.error(`Erro ao obter os produtos`, error.message);
-      next(error);
-  }
+	try {
+		res.json(await produtoService.getProdutos(req.query.page));
+	} catch (err) {
+		console.error(`Erro ao obter os produtos`, err.message);
+		next(err);
+	}
+}
+
+async function getByName(req, res, next) {
+	try {
+		res.json(await produtoService.getProductsByName(req.params.name));
+	} catch (err) {
+		console.error("Erro ao obter o produto", err.message);
+		next(err);
+	}
 }
 
 async function create(req, res, next) {
-  try {
-    res.json(await produtoService.create(req.body));
-  } catch (err) {
-    console.error(`Erro ao criar os produtos`, err.message);
-    next(err);
-  }
+	try {
+		res.json(await produtoService.create(req.body));
+	} catch (err) {
+		console.error(`Erro ao criar os produtos`, err.message);
+		next(err);
+	}
 }
 
 async function update(req, res, next) {
-  try {
-    res.json(await produtoService.update(req.params.id, req.body));
-  } catch (err) {
-    console.error(`Erro ao atualizar os produtos`, err.message);
-    next(err);
-  }
+	try {
+		res.json(await produtoService.update(req.params.id, req.body));
+	} catch (err) {
+		console.error(`Erro ao atualizar os produtos`, err.message);
+		next(err);
+	}
 }
 
 async function remove(req, res, next) {
-  try {
-    res.json(await produtoService.remove(req.params.id));
-  } catch (err) {
-    console.error(`Erro ao remover os produtos`, err.message);
-    next(err);
-  }
+	try {
+		res.json(await produtoService.remove(req.params.id));
+	} catch (err) {
+		console.error(`Erro ao remover os produtos`, err.message);
+		next(err);
+	}
 }
 
 module.exports = {
-  get,
-  create,
-  update,
-  remove
+	get,
+	getByName,
+	create,
+	update,
+	remove,
 };
