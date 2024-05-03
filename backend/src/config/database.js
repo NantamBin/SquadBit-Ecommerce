83853ff94f3963a347dbf42bc97f67cc97a6faee
NantamBin <jvjobness@protonmail.com>
2024-05-02 21:14:05 -0400

    [improvement] - edição no readme e no banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME || '',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0 
